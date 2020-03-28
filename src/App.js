415a8f22d8cf6d6a2d8f78be9b61e8865b86ad00
Michal Florian <ngomau@gmail.com>
2020-03-28 22:45:30 +0100

    Introduce activeAccount state, remove isMetaMaskReady handling via effect and state

--- src/App.js
+++ src/App.js
@@ -15,22 +15,22 @@ import { AccountNotReady , MetaMaskNotReady } from './core/alerts'
 
 const App = ({ projectTitle }) => {
 
-  const [isMetaMaskReady, setIsMetaMaskReady] = useState(false);
-  useEffect(() => { setIsMetaMaskReady(window.ethereum && window.ethereum.isMetaMask) }, []);
+  const isMetaMaskReady = window.ethereum && window.ethereum.isMetaMask;
 
-  const [isAccountReady, setIsAccountReady] = useState(false);
-  useEffect(() => {
-    ethereum.on('accountsChanged', function (accounts) {
-      setIsAccountReady(!!accounts.length);
-    })
-  }, []);
+  const [activeAccount, setActiveAccount] = useState('');
+
+  useEffect(() => { isMetaMaskReady && setActiveAccount(window.ethereum.selectedAddress); }, []);
+
+  window.ethereum.on('accountsChanged', function (accounts) {
+    setActiveAccount(accounts ? accounts[0] : '');
+  });
 
   return (
     <Router>
       <div className="container">
         <Header title={projectTitle} />
-        {isMetaMaskReady ? !isAccountReady && <AccountNotReady /> : <MetaMaskNotReady />}
-        {isAccountReady && (
+        {isMetaMaskReady ? !activeAccount && <AccountNotReady /> : <MetaMaskNotReady />}
+        {activeAccount && (
           <Fragment>
             <Navigation />
             <Switch>
