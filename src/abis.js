da1ce0c33d7bb68446c35ddd020720d86fb14781
Vladyslav Lupashevskyi <vlad@lupashevskyi.com>
2020-03-28 15:45:48 +0100

    Introduce basic blockchain interface for smart contracts

---
+++ src/abis.js
@@ -0,0 +1,394 @@
+immunityCertificateAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "registry",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "_description",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "bytes32",
+				"name": "personHash",
+				"type": "bytes32"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "tester",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "string",
+				"name": "testKitId",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "expiryTimestamp",
+				"type": "uint256"
+			}
+		],
+		"name": "CertificateIssued",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "bytes32",
+				"name": "personHash",
+				"type": "bytes32"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "tester",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "string",
+				"name": "testKitId",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "msgSender",
+				"type": "address"
+			}
+		],
+		"name": "CertificateRevoked",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "personHash",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "uint256",
+				"name": "sampleTimestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "expiryTimestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "testKitId",
+				"type": "string"
+			}
+		],
+		"name": "issue",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "personHash",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "uint256",
+				"name": "certIndex",
+				"type": "uint256"
+			}
+		],
+		"name": "revoke",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "description",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "personHash",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "uint256",
+				"name": "certIndex",
+				"type": "uint256"
+			}
+		],
+		"name": "getCertificate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "sampleTimestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "expiryTimestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "testKitId",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "tester",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "revoked",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "personHash",
+				"type": "bytes32"
+			}
+		],
+		"name": "getCertificateAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "personHash",
+				"type": "bytes32"
+			}
+		],
+		"name": "getLastCertificate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "sampleTimestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "expiryTimestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "testKitId",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "tester",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "revoked",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "registryContract",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+registryAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "tester",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "id",
+				"type": "string"
+			}
+		],
+		"name": "approve",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "tester",
+				"type": "address"
+			}
+		],
+		"name": "revoke",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address[]",
+				"name": "authorities",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "tester",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "authority",
+				"type": "address"
+			}
+		],
+		"name": "TesterApproved",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "tester",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "authority",
+				"type": "address"
+			}
+		],
+		"name": "TesterRevoked",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "isApprovingAuthority",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "isTester",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "testerId",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
--- src/blockchain.js
+++ src/blockchain.js
@@ -1,5 +1,6 @@
 
-const Web3 = require("web3");
+import Web3 from 'web3';
+import './abis';
 
 const ethEnabled = async () => {
   if (window.ethereum) {
@@ -10,9 +11,40 @@ const ethEnabled = async () => {
     catch {
         return false;
     }
+    onEthEnabled();
     return true;
   }
   return false;
 }
+
+const onEthEnabled = () => {
+    const registryContract = new web3.eth.Contract(registryAbi, '0x98EBC26814A37148e12eef1C5905Ae1EcB348f8F');
+    const certificateContract = new web3.eth.Contract(immunityCertificateAbi, '0xf0037db789d43d93BAfa6Df8e56a3813d4bF367a');
+
+    const getSender = async () => ((await web3.eth.getAccounts())[0]);
+    
+    const isAuthority = async (address) => (await registryContract.methods.isApprovingAuthority(address).call());
+    const isTester = async (address) => (await registryContract.methods.isTester(address).call());
+    const getTesterId = async (address) => (await registryContract.methods.testerId(address).call());
+    const approveTester = async (testerAddress, testerId) => (await registryContract.methods.approve(testerAddress, testerId).send({from: await getSender()}));
+    
+    const getLastCertificate = async (address) => {
+        try {
+            return await certificateContract.methods.getLastCertificate(address).call();
+        } catch {
+            return null;
+        }
+    };
+    const issueCertificate = async (personHash, sampleTimestamp, expiryTimestamp, testKitId) => (
+        await certificateContract.methods.issue(personHash, sampleTimestamp, expiryTimestamp, testKitId).send({from: await getSender()})
+    );
+
+    window.isAuthority = isAuthority;
+    window.approveTester = approveTester;
+    window.isTester = isTester;
+    window.getTesterId = getTesterId;
+    window.getLastCertificate = getLastCertificate;
+    window.issueCertificate = issueCertificate;
+};
 
 window.ethEnabled = ethEnabled;
