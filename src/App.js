db2fc08b8dd71af21d8a273b812b9361ab8d0a4b
Michal Florian <ngomau@gmail.com>
2020-03-28 14:27:31 +0100

    Convert root component to function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    const { projectTitle } = this.props;
-    return <h1>{projectTitle}</h1>;
-  }
-}
+
+const App = ({ projectTitle }) => <h1>{projectTitle}</h1>;
+
 
 export default App;
