de365c2563c3e2d477533bd0b85f1775ce4ac563
Vladyslav Lupashevskyi <vlad@lupashevskyi.com>
2020-03-28 14:37:37 +0100

    Integrate web3 with babel

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.scss";
+import "babel-polyfill";
 
+const Web3 = require("web3");
+
+const ethEnabled = async () => {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    try {
+        await window.ethereum.enable();
+    }
+    catch {
+        return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+window.ethEnabled = ethEnabled;
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App name="Jane" />, mountNode);
