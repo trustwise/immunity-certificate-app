7f01b08654ed22a9f70733c7c292b5acc0c772eb
Michal Florian <ngomau@gmail.com>
2020-03-28 18:17:40 +0100

    Introduce settings.scss, move styles imports to main stylesheet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "../node_modules/milligram/dist/milligram.min.css";
 import "./styles.scss";
 import "babel-polyfill";
 import "./blockchain";
