85cbb7dd2f767caa3728de41b8289dd6b65b7d15
Michal Florian <ngomau@gmail.com>
2020-03-29 15:09:37 +0200

    Move styles to a separate module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./styles.scss";
+import "./styles/app.scss";
 import "babel-polyfill";
 import "./blockchain";
 
