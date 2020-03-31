c56bbf89dc33b99be5888a7252cae240cb649f2c
Michal Florian <ngomau@gmail.com>
2020-04-01 01:45:54 +0200

    Move checkmark svg to a separate component

--- src/core/components/index.js
+++ src/core/components/index.js
@@ -2,5 +2,6 @@
  * Generic shared components.
  */
 
+export { default as CheckMark } from './CheckMark';
 export { default as Header } from './Header';
 export { default as Navigation } from './Navigation';
--- src/views/CheckImmunityView/result.js
+++ src/views/CheckImmunityView/result.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CheckMark from "../core/components/CheckMark"
 
 const CertificateResult = ({ passportId, testerId, testerName, validityDate }) => (
   <div className="row">
@@ -6,10 +7,7 @@ const CertificateResult = ({ passportId, testerId, testerName, validityDate }) =
       <br />
       <h3>COVID19</h3>
       <br />
-      <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-        <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
-        <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-      </svg>
+      <CheckMark size="large"/>
       <br />
       <span className="light-grey">Passport ID</span>
       <h4>{passportId}</h4>
