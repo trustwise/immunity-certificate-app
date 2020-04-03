40778ed83fffc2df503bcbe895ccb42b5c0d274a
Michal Florian <ngomau@gmail.com>
2020-04-03 04:06:39 +0200

    Remove unused utility method

--- src/core/utils.js
+++ src/core/utils.js
@@ -4,6 +4,4 @@
 
 export const generatePepper = (length) => (Math.random().toString(36) + '00000000000000000').slice(2, length + 2).toUpperCase();
 
-export const isEmptyObject = (obj) => Object.keys(obj).length === 0 && obj.constructor === Object;
-
 export const formatDate = (date) => `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
