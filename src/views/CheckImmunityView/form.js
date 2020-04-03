4a125609974d36ef4a0c332f3c3f13bb2509e3ea
Michal Florian <ngomau@gmail.com>
2020-04-03 15:47:12 +0200

    Fix async submission handling in CheckImmunityForm

--- src/views/CheckImmunityView/form.js
+++ src/views/CheckImmunityView/form.js
@@ -7,50 +7,58 @@ import { LegacyQrReader } from '/core/components';
 import { SEPARATOR } from "/core/constants";
 
 
-const CheckImmunityForm = ({setCertificate, setIsCertificateFetched, resultRef}) => (
-  <Formik
-    initialValues={{
-      personalCode: '',
-    }}
-    validationSchema={Yup.object({
-      personalCode: Yup.string().required('This field is required'),
-    })}
-    onSubmit={(values, { setSubmitting }) => {
-      const personalCode = Web3.utils.sha3(`${values.personalCode}`);
-      getLastCertificate(personalCode).then((result) => {
-        if (result) {
-          getTesterId(result.tester).then((tester) => {
-            const [testerName, testerId] = tester.split(SEPARATOR);
-            const [passportId, _pepper] = values.personalCode.split(SEPARATOR);
-            result.expired = result.expiryTimestamp < Math.floor(Date.now() / 1000);
-            result.expiryDate = new Date(result.expiryTimestamp * 1000);
-            result.sampleDate = new Date(result.sampleTimestamp * 1000);
-            result.passportId = passportId;
-            result.testerId = testerId;
-            result.testerName = testerName;
-          });
-          resultRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+const CheckImmunityForm = ({setCertificate, setIsCertificateFetched, resultRef}) => {
+  const onSubmitFinished = (result, setSubmitting) => {
+    setCertificate(result);
+    setIsCertificateFetched(true);
+    setSubmitting(false);
+  };
+  return (
+    <Formik
+      initialValues={{
+        personalCode: '',
+      }}
+      validationSchema={Yup.object({
+        personalCode: Yup.string().required('This field is required'),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        const personalCode = Web3.utils.sha3(`${values.personalCode}`);
+        getLastCertificate(personalCode).then((result) => {
+          try {
+            getTesterId(result.tester).then((tester) => {
+              const [testerName, testerId] = tester.split(SEPARATOR);
+              const [passportId, _pepper] = values.personalCode.split(SEPARATOR);
+              result.expired = result.expiryTimestamp < Math.floor(Date.now() / 1000);
+              result.expiryDate = new Date(result.expiryTimestamp * 1000);
+              result.sampleDate = new Date(result.sampleTimestamp * 1000);
+              result.passportId = passportId;
+              result.testerId = testerId;
+              result.testerName = testerName;
+              onSubmitFinished(result, setSubmitting);
+              resultRef.current && resultRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+            });
+          } catch (_error) {
+            onSubmitFinished(result, setSubmitting);
+          }
+        });
+      }}
+    >
+      {({ setFieldValue, submitForm }) => {
+        const onScan = (result) => {
+          if (result) {
+            // TODO handle invalid QR code
+            setFieldValue('personalCode', result);
+            submitForm();
+          }
         }
-        setCertificate(result);
-        setIsCertificateFetched(true);
-      });
-      setSubmitting(false);
-    }}
-  >
-    {({ setFieldValue, submitForm }) => {
-      const onScan = (result) => {
-        if (result) {
-          setFieldValue('personalCode', result);
-          submitForm();
-        }
-      }
-      return (
-        <Form>
-          <LegacyQrReader onScan={onScan} />
-        </Form>
-      );
-    }}
-  </Formik>
-);
+        return (
+          <Form>
+            <LegacyQrReader onScan={onScan} />
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
 
 export default CheckImmunityForm;
