40f427159f4eca996bcda5f4f1a7d8539f1da7bb
Michal Florian <ngomau@gmail.com>
2020-04-04 01:42:52 +0200

    Simplify check immunity form, remove unnecessary formik related code

--- src/views/CheckImmunityView/form.js
+++ src/views/CheckImmunityView/form.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
 import Web3 from "web3";
 
 import { LegacyQrReader } from '/core/components';
@@ -8,57 +6,36 @@ import { SEPARATOR } from "/core/constants";
 
 
 const CheckImmunityForm = ({setCertificate, setIsCertificateFetched, resultRef}) => {
-  const handleSubmitFinished = (result, setSubmitting) => {
+  const handleSubmitFinished = (result) => {
     setCertificate(result);
     setIsCertificateFetched(true);
-    setSubmitting(false);
   };
-  return (
-    <Formik
-      initialValues={{
-        personalCode: '',
-      }}
-      validationSchema={Yup.object({
-        personalCode: Yup.string().required('This field is required'),
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        const personalCode = Web3.utils.sha3(`${values.personalCode}`);
-        getLastCertificate(personalCode).then((result) => {
-          try {
-            getTesterId(result.tester).then((tester) => {
-              const [testerName, testerId] = tester.split(SEPARATOR);
-              const [passportId, _pepper] = values.personalCode.split(SEPARATOR);
-              result.expired = result.expiryTimestamp < Math.floor(Date.now() / 1000);
-              result.expiryDate = new Date(result.expiryTimestamp * 1000);
-              result.sampleDate = new Date(result.sampleTimestamp * 1000);
-              result.passportId = passportId;
-              result.testerId = testerId;
-              result.testerName = testerName;
-              handleSubmitFinished(result, setSubmitting);
-              resultRef.current && resultRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
-            });
-          } catch (_error) {
-            handleSubmitFinished(result, setSubmitting);
-          }
+  const handleQrScan = (personalCode) => {
+    // TODO handle invalid QR code
+    if (!personalCode) {
+      return null;
+    }
+    const personalCodeHash = Web3.utils.sha3(`${personalCode}`);
+    getLastCertificate(personalCodeHash).then((result) => {
+      try {
+        getTesterId(result.tester).then((tester) => {
+          const [testerName, testerId] = tester.split(SEPARATOR);
+          const [passportId, _pepper] = personalCode.split(SEPARATOR);
+          result.expired = result.expiryTimestamp < Math.floor(Date.now() / 1000);
+          result.expiryDate = new Date(result.expiryTimestamp * 1000);
+          result.sampleDate = new Date(result.sampleTimestamp * 1000);
+          result.passportId = passportId;
+          result.testerId = testerId;
+          result.testerName = testerName;
+          handleSubmitFinished(result);
+          resultRef.current && resultRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
         });
-      }}
-    >
-      {({ setFieldValue, submitForm }) => {
-        const handleQrScan = (result) => {
-          if (result) {
-            // TODO handle invalid QR code
-            setFieldValue('personalCode', result);
-            submitForm();
-          }
-        }
-        return (
-          <Form>
-            <LegacyQrReader onScan={handleQrScan} />
-          </Form>
-        );
-      }}
-    </Formik>
-  );
+      } catch (_error) {
+        handleSubmitFinished(result);
+      }
+    });
+  };
+  return <LegacyQrReader onScan={handleQrScan} />;
 };
 
 export default CheckImmunityForm;
